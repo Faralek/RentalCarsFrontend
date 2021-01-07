1f6930897eb32da2dd9cc2d985f5ee10f19af967
48727 <swierczynski093@gmail.com>
2021-01-07 20:11:04 +0100

    unversioned files commit

---
+++ src/main/java/com/kodilla/rentalcars/frontend/RentalCarsFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.rentalcars.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalCarsFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalCarsFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/rentalcars/frontend/RentalCarsFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.rentalcars.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalCarsFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
